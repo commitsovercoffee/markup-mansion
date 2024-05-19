4f125be28b275fba0427d38701d49c6a2f597410
commitsovercoffee <commitsovercoffee@gmail.com>
2024-05-19 22:15:35 +0530

    adds sitemap & nav bar

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,65 @@
+<script>
+	import { draw, fly } from 'svelte/transition';
+	import { quintOut } from 'svelte/easing';
+	import { page } from '$app/stores';
+
+	$: currentURL = $page.url.pathname;
+
+	import { onMount } from 'svelte';
+
+	const links = ['home', 'writing', 'apps'];
+
+	export let hidden = false;
+
+	onMount(() => {
+		setTimeout(() => {
+			hidden = true;
+		}, 1500);
+	});
+</script>
+
+<div class="flex justify-between items-center w-full mt-16 mb-16">
+	<div class="h-16 w-16">
+		{#if hidden}
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="100%"
+				height="100%"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="currentColor"
+				stroke-width="1"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				class="lucide lucide-coffee stroke-red-400"
+			>
+				<path transition:draw={{ duration: 1500, easing: quintOut }} d="M10 2v2" />
+				<path transition:draw={{ duration: 1500, easing: quintOut }} d="M14 2v2" />
+				<path
+					transition:draw={{ duration: 1500, easing: quintOut }}
+					d="M16 8a1 1 0 0 1 1 1v8a4 4 0 0 1-4 4H7a4 4 0 0 1-4-4V9a1 1 0 0 1 1-1h14a4 4 0 1 1 0 8h-1"
+				/>
+				<path transition:draw={{ duration: 1500, easing: quintOut }} d="M6 2v2" />
+			</svg>
+		{/if}
+	</div>
+	<div class="list-none flex gap-4">
+		{#each links as link}
+			{#if hidden}
+				<a
+					href={link === 'home' ? '/' : link}
+					class="{currentURL === '/' && link === 'home'
+						? 'text-red-400'
+						: currentURL.slice(1) === link &&
+							'text-red-400'} no-underline hover:text-red-400 group transition-all duration-200 delay-75"
+					transition:fly={{ delay: 1000, duration: 1500, easing: quintOut }}
+				>
+					{link}
+					<span
+						class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-red-400"
+					></span>
+				</a>
+			{/if}
+		{/each}
+	</div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script>
+	import Nav from '$lib/components/Nav.svelte';
 	import '../app.css';
 
 	// Supports weights 100-900
 	import '@fontsource-variable/overpass';
 </script>
 
+<Nav />
 <slot />

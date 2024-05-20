6f9fc5671728289b17b5015b2a7e2ed15f96f191
commitsovercoffee <commitsovercoffee@gmail.com>
2024-05-20 19:01:21 +0530

    adds static adapter

--- src/lib/components/App.svelte
+++ src/lib/components/App.svelte
@@ -5,12 +5,19 @@
 		'Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.';
 </script>
 
-<div class="my-6 rounded-xl bg-[#202127] flex justify-start items-start">
-	<div class="rounded-lg m-4 p-4 bg-[#1b1b1f] hidden md:block">
+<div class="my-6 rounded-xl bg-[#32363f] flex justify-start items-start">
+	<div class="rounded-lg m-4 p-4 hidden md:block">
 		<slot />
 	</div>
 	<div class="m-4">
-		<a href="https://github.com/commitsovercoffee/{repo}" class="font-semibold">{title}</a>
-		<p class="m-0 mt-2 leading-tight">{desc}</p>
+		<a
+			href="https://github.com/commitsovercoffee/{repo}"
+			class="prose-neutral prose-invert no-underline font-bold group transition-all duration-200"
+		>
+			{title}
+			<span class="block max-w-4 group-hover:max-w-32 transition-all duration-500 h-0.5 bg-red-400"
+			></span>
+		</a>
+		<p class="m-0 mt4 text-neutral-400">{desc}</p>
 	</div>
 </div>

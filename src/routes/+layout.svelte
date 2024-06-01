5b7e0b0d9b4e760c3f2e26e938df3927618b39d6
commitsovercoffee <commitsovercoffee@gmail.com>
2024-06-01 08:48:17 +0530

    adds loader

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,16 +5,36 @@
 
 	import Nav from '$lib/components/Nav.svelte';
 
-	import { fade } from 'svelte/transition';
+	import { fly } from 'svelte/transition';
+	import { cubicIn, cubicOut } from 'svelte/easing';
 	import Footer from '$lib/components/Footer.svelte';
 
+	import { beforeNavigate, afterNavigate } from '$app/navigation';
+	import Loader from '$lib/components/Loader.svelte';
+
 	export let data;
+
+	let isLoading = false;
+	beforeNavigate(() => (isLoading = true));
+	afterNavigate(() => (isLoading = false));
 </script>
 
 <Nav />
+<!--
+A #key block takes an expression, and automatically re-renders its contents
+whenever that expression changes.
+-->
 {#key data.currentRoute}
-	<main in:fade={{ duration: 300, delay: 150 }} out:fade={{ duration: 150 }}>
-		<slot />
+	<main
+		in:fly={{ easing: cubicOut, y: 10, duration: 300, delay: 400 }}
+		out:fly={{ easing: cubicIn, y: -10, duration: 300 }}
+	>
+		{#if isLoading}
+			<Loader />
+		{:else}
+			<slot />
+		{/if}
 	</main>
 {/key}
+
 <Footer />

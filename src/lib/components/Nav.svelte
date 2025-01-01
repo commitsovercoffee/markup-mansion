3b8ad52293cf7cea5dd5a25b01c774d4bfdb3ea9
commitsovercoffee <commitsovercoffee@gmail.com>
2025-01-02 02:19:30 +0530

    adds socials component

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,48 +1,71 @@
 <script>
 	import { selected, themes } from '$lib/theme.svelte';
 	import { page } from '$app/state';
+	import Socials from './Socials.svelte';
 
 	const links = ['blog', 'apps'];
+	let over = $state('none');
 </script>
 
-<nav class="flex flex-row items-center justify-between py-16">
-	<a
-		style:color={themes[selected].foreground}
-		class=" flex flex-row gap-2 p-2 no-underline"
-		href="/"
-	>
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			width="24"
-			height="24"
-			viewBox="0 0 24 24"
-			fill="none"
-			stroke="currentColor"
-			stroke-width="1"
-			stroke-linecap="round"
-			stroke-linejoin="round"
-			class="lucide lucide-coffee -rotate-12"
-			><path d="M10 2v2" /><path d="M14 2v2" /><path
-				d="M16 8a1 1 0 0 1 1 1v8a4 4 0 0 1-4 4H7a4 4 0 0 1-4-4V9a1 1 0 0 1 1-1h14a4 4 0 1 1 0 8h-1"
-			/><path d="M6 2v2" /></svg
+<nav class="flex flex-row justify-between">
+	<div class="flex flex-row items-center gap-2 py-16">
+		<a
+			style:color={themes[selected].foreground}
+			class="p2 group flex flex-row items-center gap-2 no-underline"
+			href="/"
+			onmouseover={() => {
+				over = 'home';
+			}}
+			onmouseleave={() => {
+				over = 'none';
+			}}
+			onfocus={() => {}}
 		>
-		<span> CommitsOverCoffee </span>
-	</a>
-	<div>
+			<svg
+				width="100%"
+				height="100%"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="currentColor"
+				stroke-width="1.2"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				class="h-8 w-8 -rotate-12 transition-all duration-100 ease-in group-hover:rotate-0"
+				><path d="M10 2v2" /><path d="M14 2v2" /><path
+					d="M16 8a1 1 0 0 1 1 1v8a4 4 0 0 1-4 4H7a4 4 0 0 1-4-4V9a1 1 0 0 1 1-1h14a4 4 0 1 1 0 8h-1"
+				/><path d="M6 2v2" /></svg
+			>
+			<span
+				class="p-2"
+				style:color={page.url.pathname === '/'
+					? themes[selected].foreground
+					: themes[selected].desc}
+			>
+				CommitsOverCoffee
+			</span>
+		</a>
 		{#each links as link}
-			{#if link === 'home'}{:else}
-				<a
-					style:color={page.url.pathname.slice(1) === link
-						? themes[selected].primary
-						: themes[selected].foreground}
-					class="p-2 no-underline"
-					href={link}
-				>
-					{link}</a
-				>
-			{/if}
+			<a
+				onmouseover={() => {
+					over = link;
+				}}
+				onmouseleave={() => {
+					over = 'none';
+				}}
+				onfocus={() => {}}
+				style:color={page.url.pathname.slice(1) === link
+					? themes[selected].foreground
+					: over === link
+						? themes[selected].foreground
+						: themes[selected].desc}
+				class="p-2 no-underline"
+				href={link}
+			>
+				{link}</a
+			>
 		{/each}
 	</div>
+	<Socials />
 </nav>
 
 <style>

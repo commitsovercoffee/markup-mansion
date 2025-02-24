396ced45f99e1ad254d6f3ff4a37f65a3b844a6f
commitsovercoffee <commitsovercoffee@gmail.com>
2025-02-24 08:00:37 +0530

    styles nav bar

---
+++ src/lib/theme.svelte.js
@@ -0,0 +1,23 @@
+export const theme = $state('aura');
+export const palette = {
+	aura: {
+		background: '#110F18',
+		foreground: '#EDECEE',
+		panel: '#1f2430',
+
+		primary: '#FF6767',
+		secondary: '#FFCA85',
+		highlight: '#61FFCA',
+		desc: '#6D6D6D',
+		link: '#A277FF'
+	},
+	field: {
+		background: '#18201e',
+		foreground: '#8ea4a0',
+		primary: '#3be381',
+		secondary: '#0fbda0',
+		highlight: '#a8fff1',
+		desc: '#5a6d6a',
+		link: '#88f2e0'
+	}
+};
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,21 @@
 <script>
 	import '../app.css';
 	import '@fontsource-variable/overpass'; // 100-900
+
 	import Nav from '$lib/Nav.svelte';
 
 	let { children } = $props();
+
+	import { theme, palette } from '$lib/theme.svelte';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		document.body.style.backgroundColor = palette[theme].background;
+		document.body.style.fontFamily = 'Overpass Variable';
+	});
 </script>
 
-<Nav />
-{@render children()}
+<main style="" class="prose prose-invert mx-auto">
+	<Nav />
+	{@render children()}
+</main>
